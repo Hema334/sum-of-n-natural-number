cad133fd856f9b9a3075bf76104dd1d325e16cc0
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-08 11:39:34 +0530

    Add files via upload

---
+++ sum of natural numbers.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+int main()
+{
+   int i,n,sum=0;
+   printf("enter the value of n:");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       sum=sum+i;
+   }
+   printf("sum= %d",sum);
+}
